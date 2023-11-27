548b337f2e54f85eb64e01e9488d4b0cf0e0c443
joão vitor <shepherdcom12@gmail.com>
2023-11-27 13:21:16 -0300

    update 1.2.1

--- src/script/script.js
+++ src/script/script.js
@@ -18,6 +18,7 @@ const openMenu = document.getElementById("openMenu")
 const menuSection = document.getElementById("menuSection")
 const closeMenu = document.getElementById("closeMenu")
 const darkLightBtn = document.getElementById("darkLightBtn")
+const menu_Search = document.getElementById("menu_Search")
 
 darkLightBtn.addEventListener("click", () => {
     document.querySelector("body").classList.toggle("dark")
@@ -59,53 +60,80 @@ function desactiveLoading() {
 const numberOfTrack = 0
 
 async function initApp(musicId) {
-    const url = `https://spotify81.p.rapidapi.com/tracks?ids=${musicId}`;
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'c25b38504amsh07c3b69988a18f8p1c1ba7jsn54720b39dbba',
-            'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
-        }
-    };
+    console.log(musicId);
 
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        console.log(result.tracks[0]);
-        MusicName.textContent = `${result.tracks[0].name}`
-        ArtistName.textContent = `${result.tracks[0].album.artists[0].name}`
-        musicLoader.style.display = "none"
-        musicTime.style.display = "flex"
-        vizualizer.style.backgroundImage = `url(${result.tracks[0].album.images[0].url})`
-        audioTag.src = `${result.tracks[0].preview_url}`
-        spotfyLink.href = `${result.tracks[0].external_urls.spotify}`
-        musicPlayPause.onclick = () => {
-            if (!audioTag.paused) {
-                audioTag.pause()
-                musicPlayPause.children[0].name = "play"
-            } else {
-                audioTag.play()
-                musicPlayPause.children[0].name = "pause"
+    async function trackData() {
+        const url = `https://spotify81.p.rapidapi.com/tracks?ids=${musicId}`;
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'c25b38504amsh07c3b69988a18f8p1c1ba7jsn54720b39dbba',
+                'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
             }
+        };
+
+        try {
+            const response = await fetch(url, options);
+            const result = await response.json();
+            MusicName.textContent = `${result.tracks[0].name}`
+            ArtistName.textContent = `${result.tracks[0].album.artists[0].name}`
+            vizualizer.style.backgroundImage = `url(${result.tracks[0].album.images[0].url})`
+            spotfyLink.href = `${result.tracks[0].external_urls.spotify}`
+        } catch (error) {
+            console.error(error);
         }
-        forwardMusic.onclick = () => {
-            audioTag.currentTime = audioTag.currentTime + 10
-        }
-        backMusic.onclick = () => {
-            audioTag.currentTime = audioTag.currentTime - 10
-        }
-        setInterval(() => {
-            if (audioTag.currentTime == audioTag.duration) {
-                musicPlayPause.children[0].name = "play"
-            }
-            musicTime.textContent = `${`${Math.floor((audioTag.duration.toFixed(2) - audioTag.currentTime.toFixed(2)) / 60)}`.length == 1 ? `0${Math.floor((audioTag.duration.toFixed(2) - audioTag.currentTime.toFixed(2)) / 60)}` : `${Math.floor((audioTag.duration.toFixed(2) - audioTag.currentTime.toFixed(2)) / 60)}`}:${`${((audioTag.duration.toFixed(2) - audioTag.currentTime.toFixed(2)) % 60).toFixed(0)}`.length == 1 ? `0${((audioTag.duration.toFixed(2) - audioTag.currentTime.toFixed(2)) % 60).toFixed(0)}` : `${((audioTag.duration.toFixed(2) - audioTag.currentTime.toFixed(2)) % 60).toFixed(0)}`}`
-            musicDuration.style.background = `linear-gradient(180deg, var(--red) 0%, var(--black) ${((audioTag.currentTime / audioTag.duration) * 100).toFixed(2)}%)`
-        }, 100);
-        searchSection.style.display = "none"
-        desactiveLoading()
-    } catch (error) {
-        console.error(error);
     }
+
+
+    async function completeTrack() {
+        let url = `https://spotify81.p.rapidapi.com/download_track?q=${musicId}&onlyLinks=1`;
+        let options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'c25b38504amsh07c3b69988a18f8p1c1ba7jsn54720b39dbba',
+                'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
+            }
+        };
+
+        try {
+            let response = await fetch(url, options);
+            let result = await response.json();
+            audioTag.src = `${result[0].url}`
+            audioTag.addEventListener('error', function () {
+                console.error('Erro ao carregar o áudio. Verifique o src ou a conexão de rede.');
+            });
+            musicLoader.style.display = "none"
+            musicTime.style.display = "flex"
+            musicPlayPause.onclick = () => {
+                if (!audioTag.paused) {
+                    audioTag.pause()
+                    musicPlayPause.children[0].name = "play"
+                } else {
+                    audioTag.play()
+                    musicPlayPause.children[0].name = "pause"
+                }
+            }
+            forwardMusic.onclick = () => {
+                audioTag.currentTime = audioTag.currentTime + 10
+            }
+            backMusic.onclick = () => {
+                audioTag.currentTime = audioTag.currentTime - 10
+            }
+            setInterval(() => {
+                if (audioTag.currentTime == audioTag.duration) {
+                    musicPlayPause.children[0].name = "reload-outline"
+                }
+                musicTime.textContent = `${`${Math.floor((audioTag.duration.toFixed(2) - audioTag.currentTime.toFixed(2)) / 60)}`.length == 1 ? `0${Math.floor((audioTag.duration.toFixed(2) - audioTag.currentTime.toFixed(2)) / 60)}` : `${Math.floor((audioTag.duration.toFixed(2) - audioTag.currentTime.toFixed(2)) / 60)}`}:${`${((audioTag.duration.toFixed(2) - audioTag.currentTime.toFixed(2)) % 60).toFixed(0)}`.length == 1 ? `0${((audioTag.duration.toFixed(2) - audioTag.currentTime.toFixed(2)) % 60).toFixed(0)}` : `${((audioTag.duration.toFixed(2) - audioTag.currentTime.toFixed(2)) % 60).toFixed(0)}`}`
+                musicDuration.style.background = `linear-gradient(180deg, var(--red) 0%, var(--black) ${((audioTag.currentTime / audioTag.duration) * 100).toFixed(2)}%)`
+            }, 100);
+            searchSection.style.display = "none"
+            desactiveLoading()
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    trackData()
+    completeTrack()
 }
 
 
@@ -141,8 +169,18 @@ searchBtn.addEventListener("click", () => {
     musicPlayPause.children[0].name = "play"
 })
 
-
-
+menu_Search.addEventListener("click", () => {
+    searchSection.style.display = "flex"
+    audioTag.pause()
+    audioTag.currentTime = 0
+    audioTag.src = ""
+    musicTime.textContent = ""
+    musicPlayPause.children[0].name = "play"
+    menuSection.style.opacity = "0"
+    setTimeout(() => {
+        menuSection.style.display = "none"
+    }, 500);
+})
 
 
 
